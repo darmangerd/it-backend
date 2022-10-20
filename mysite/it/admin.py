878699f6035653940bf4f3e4fe2987b24a3754e3
David Darmanger <david.darmanger@he-arc.ch>
2022-10-20 09:07:09 +0200

    Correct model + Finish fixtures

--- mysite/it/admin.py
+++ mysite/it/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Client, Meal, Food, Quantity
 
-# Register your models here.
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Meal._meta.get_fields()]
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Client._meta.get_fields()]
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Food._meta.get_fields()]
+
+
+@admin.register(Quantity)
+class QuantityAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Quantity._meta.get_fields()]
--- mysite/it/models/meal.py
+++ mysite/it/models/meal.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Meal(models.Model):
     id = models.AutoField(primary_key=True)
     date = models.DateField()
     id_user = models.ForeignKey(
-        "Client",
+        User,
         on_delete=models.CASCADE,
     )
