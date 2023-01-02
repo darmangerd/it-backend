ed25faa26e3a62cbf80a59b5b36f8dacdcbf56d6
David Darmanger <david.darmanger@he-arc.ch>
2023-01-02 17:24:33 +0100

    make food name unique

--- mysite/it/models/food.py
+++ mysite/it/models/food.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Food(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     energy_kcal = models.IntegerField()
     proteins_g = models.FloatField()
     lipids_g = models.FloatField()
