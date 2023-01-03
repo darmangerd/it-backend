83f824a35bb21ba104e0e2e738239cfa70131d2b
David Darmanger <david.darmanger@he-arc.ch>
2023-01-03 19:08:38 +0100

    unfinished get user id

--- mysite/it/views/user.py
+++ mysite/it/views/user.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import login
 from django.contrib.auth.models import User
-
+from rest_framework.response import Response
 from rest_framework import viewsets, permissions
 from rest_framework import permissions
 from rest_framework.authtoken.serializers import AuthTokenSerializer
@@ -10,6 +10,7 @@ from knox.views import LoginView as KnoxLoginView
 from ..serializers import UserSerializer
 
 from drf_yasg.utils import swagger_auto_schema
+from django.contrib.auth.models import User
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -21,6 +22,34 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
     swagger_tag = ["User"]
+
+    # return the pk from the username
+    def get_queryset(self):
+        username = self.request.query_params.get("username")
+        queryset = User.objects.all()
+        if username:
+            queryset = queryset.filter(username=username)
+            # return pk
+            return queryset.values("id", "username")
+        return queryset
+
+    # def list(self, request):
+    #     username = request.query_params.get("username")
+    #     if username:
+    #         queryset = self.queryset.filter(username=username)
+    #     else:
+    #         queryset = self.queryset
+    #     serializer = self.serializer_class(queryset, many=True)
+    #     return Response(serializer.data)
+
+    # def retrieve(self, request, pk=None):
+    #     try:
+    #         user = self.queryset.get(pk=pk)
+    #         serializer = self.serializer_class(user)
+    #         return Response(serializer.data)
+
+    #     except User.DoesNotExist:
+    #         return Response({})
 
 
 class LoginView(KnoxLoginView):
